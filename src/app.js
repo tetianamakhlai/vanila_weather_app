4b7755c1c634c66f0bee2578737fcc9d21c050e5
tetianamakhlai <tetianamakhlai@gmail.com>
2023-06-18 17:28:39 +0200

    added time function with time delay
    trying to figure out the reason of time delay

--- src/app.js
+++ src/app.js
@@ -11,9 +11,52 @@ function displayTemperature(response) {
   humidity.innerHTML = Math.round(response.data.main.humidity);
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000); // Qustion!! the time is late . Why? Why in another lesson we didnt use timestamp parameter
 }
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hrs = date.getHours();
+  let minuts = date.getMinutes();
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayOfTheWeek = days[date.getDay()];
+  if (hrs < 10) {
+    ` 0${hrs}`;
+  }
+  if (minuts < 10) {
+    `0${minuts}`;
+  }
+  return `${dayOfTheWeek}, ${hrs}:${minuts}`;
+}
+// What is the difference with the second option of setting a time
+//let currentTime = new Date();
+/* const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let dayOfWeek = days[currentTime.getDay()];
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let time = `${dayOfWeek},${hours}:${minutes}`;
+document.querySelector("#currentTime").innerHTML = time; */
+
 let apiKey = "3b304f6fbf39e6bd217e9118b5adafbf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
