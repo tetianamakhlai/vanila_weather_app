883eaf8a8bf0e71d10de921dd8cd6a8f42f895b8
tetianamakhlai <tetianamakhlai@gmail.com>
2023-06-18 12:12:50 +0200

    Showing the current temperature
    and other elements

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
-let apiKey = "3b304f6fbf39e6bd217e9118b5adafbf";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-let currentTemp = document.querySelector("#current_tempr");
-function displayData(responce) {
-  currentTemp.innerHTML = Math.round(responce.data.main.temp);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperatures");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#main_city");
+  city.innerHTML = response.data.name;
+  let feelsLike = document.querySelector("#feels_like");
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = Math.round(response.data.main.humidity);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
 }
 
-axios.get(apiUrl).then(displayData);
+let apiKey = "3b304f6fbf39e6bd217e9118b5adafbf";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
